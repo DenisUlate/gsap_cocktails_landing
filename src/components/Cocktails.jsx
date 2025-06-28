924b3b3f9d2394348c1d63b410093f0a99afb3de
Denis Ulate <92893770+DenisUlate@users.noreply.github.com>
2025-06-27 21:10:58 -0600

    Fix: correct import path for constants and update image source paths in Cocktails component

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,4 +1,4 @@
-import { cocktailLists, mockTailLists } from "../constants";
+import { cocktailLists, mockTailLists } from "../constants/index.js";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
@@ -20,8 +20,8 @@ const Cocktails = () => {
 
 	return (
 		<section id="cocktails" className="noisy">
-			<img src="/public/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
-			<img src="/public/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+			<img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+			<img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
 
 			<div className="list">
 				<div className="popular">
